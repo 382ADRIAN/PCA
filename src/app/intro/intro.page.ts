545dc29a70a14c64a3be5e1552c61d3af4f00fc0
382ADRIAN <93597769+382ADRIAN@users.noreply.github.com>
2023-07-10 22:44:47 -0500

    adelanto tarea

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-
+import { Storage } from '@ionic/storage-angular';
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
@@ -13,25 +13,44 @@ export class IntroPage implements OnInit {
       title:"Perro",
       img: "assets/image/perro.webp",
       icon: "beer-outline",
-      description: "Hola soy la explicación del slide"
+      description: "esta raza de perro son demasiado activo,cariñoso y muy obediente etc"
     },
     {
       title:"Gato",
       img: "assets/image/gato.webp",
       icon: "barbell-outline",
-      description: "Hola soy la explicación del slide"
+      description: "esta raza de gato son muy casadores"
+    },
+    {
+      title:"buho",
+      img: "assets/image/buho.jpg",
+      icon: "barbell-outline",
+      description: "esta ave es nocturna, buena casadoras"
+    }, 
+    {
+      title:"elefante",
+      img: "assets/image/elefante.jpg",
+      icon: "barbell-outline",
+      description: "este tipo de animales son super protectores con la crias"
+    },
+     {
+      title:"tigre",
+      img: "assets/image/tigre.avif",
+      icon: "barbell-outline",
+      description: "esta raza de gaton son muy casadores"
     }
   ]
   
   
-  constructor(private router: Router) { }
+  constructor(private router: Router, private storage: Storage) { }
 
   ngOnInit() {
   }
 
   finish(){
+    this.storage.set("introShow", true);
     this.router.navigateByUrl("/home");
-    console.log("salir")
+
 
 }
 }
